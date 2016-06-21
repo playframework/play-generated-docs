2f2cf8a7e629ba35ce2a09b865bc91d034b390cb
Play Team <play@vegemite.typesafe.com>
2016-06-21 06:19:19 -0700

    Update documentation

--- manual/working/scalaGuide/main/templates/code/ScalaTemplates.scala
+++ manual/working/scalaGuide/main/templates/code/ScalaTemplates.scala
@@ -6,26 +6,22 @@ package scalaguide.templates
 import org.specs2.mutable.Specification
 import play.twirl.api.Html
 
-// These have to be in the same package as the template
-package views.html.Application {
-  case class Order(title: String)
-  case class Customer(name: String)
+case class Order(title: String)
+case class Customer(name: String)
+case class Product(name: String, price: String)
+case class User(firstName: String, lastName: String)
+case class Article(content: String)
+
+case class MyComponent() {
+  override def toString = "MyComponent"
 }
 
-package html.models {
-  case class Order(title: String)
-  case class Customer(name: String)
-  case class Product(name: String, price: String)
-  case class User(firstName: String, lastName: String)
-  case class Article(content: String)
+case class MyFieldConstructor() {
+  val working = "implicit working"
+}
 
-  case class MyFieldConstructor() {
-    val working = "implicit working"
-  }
-
-  object ImplicitTester {
-    def test(implicit f: MyFieldConstructor) = f.working
-  }
+object ImplicitTester {
+  def test(implicit f: MyFieldConstructor) = f.working
 }
 
 package html.utils {
@@ -36,15 +32,12 @@ package html.utils {
 
 object ScalaTemplatesSpec extends Specification {
 
-  import html.models._
-
   val customer = Customer("mr customer")
   val orders = List(Order("foo"), Order("bar"))
 
 
   "Scala templates" should {
     "support an example template" in {
-      import views.html.Application._
 
       val c = Customer("mr customer")
       val o = List(Order("foo"), Order("bar"))
@@ -57,6 +50,17 @@ object ScalaTemplatesSpec extends Specification {
       body must contain("mr customer")
       body must contain("foo")
       body must contain("bar")
+    }
+
+    "support string interpolation" in {
+      //#string-interpolation
+      import play.twirl.api.StringInterpolation
+
+      val name = "Martin"
+      val p = html"<p>Hello $name</p>"
+      //#string-interpolation
+
+      p.body must_== "<p>Hello Martin</p>"
     }
 
     "allow simple parameters" in {
@@ -75,6 +79,14 @@ object ScalaTemplatesSpec extends Specification {
       val body = html.curriedParameters("foo")(Html("bar")).body
       body must contain("foo")
       body must contain("bar")
+    }
+
+    "allow constructors" in {
+      val body = new html.constructor(MyComponent())(customer, orders).body
+      body must contain("MyComponent")
+      body must contain(customer.toString)
+      body must contain(orders(0).toString)
+      body must contain(orders(1).toString)
     }
 
     "allow import statements" in {
