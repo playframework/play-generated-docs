e05d87a212ff42704a91a965cce09162d322fb24
Play Team <play@vegemite.typesafe.com>
2019-04-25 06:40:40 -0700

    Revert "Update documentation"
    This reverts commit fc3b3bdb7d21695f093c219142048e8a2daf09a1.

---
+++ manual/working/commonGuide/filters/code/SecurityHeaders.scala
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2009-2019 Lightbend Inc. <https://www.lightbend.com>
+ */
+package detailedtopics.configuration.securityheaders
+
+//#filters
+import javax.inject.Inject
+
+import play.api.http.DefaultHttpFilters
+import play.filters.headers.SecurityHeadersFilter
+import play.api.mvc.{ BaseController, ControllerComponents }
+//#filters
+
+class SecurityHeaders @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+
+  def index = Action {
+  //#allowActionSpecificHeaders
+  	Ok("Index").withHeaders(SecurityHeadersFilter.CONTENT_SECURITY_POLICY_HEADER -> "my page-specific header")
+  //#allowActionSpecificHeaders
+  }
+}
+
+object SecurityHeaders {
+  class Filters @Inject() (securityHeadersFilter: SecurityHeadersFilter) extends DefaultHttpFilters(securityHeadersFilter)
+}
